7b9277c293e31ee09c2cd4fc9f971157580f0399
Andres20-Utec <andres.riveros@utec.edu.pe>
2021-06-01 19:04:05 -0500

    add accessMemoryTest

--- Parser/AccessMemoryTest.cpp
+++ Parser/AccessMemoryTest.cpp
@@ -30,21 +30,18 @@ void hashTest(string amount, vector<Universitario>& data, int maxheight){
   for(int i = 0; i < vu.size(); i++){
     dinamicHash.insert(vu[i]);
   }
-  for(int i = 0; i < 6; i++){
-    auto start  = chrono::steady_clock::now();
-    dinamicHash.insert(vu[i*100]);
-    auto end    = chrono::steady_clock::now();
-    auto duration = chrono::duration_cast<chrono::microseconds>(end-start).count();
-    cout <<"Add duration: " <<duration << endl;
-  }
-  for(int i = 0; i < 5; i++){
-    auto start  = chrono::steady_clock::now();
-    const char* codigo = vu[i*100].getNombre();
-    dinamicHash.search(codigo);
-    auto end    = chrono::steady_clock::now();
-    auto duration = chrono::duration_cast<chrono::microseconds>(end-start).count();
-    cout <<"Search duration: " <<duration << endl;
-  }
+  /*
+  int prev = dinamicHash.get();
+  dinamicHash.insert(vu[100]);
+  int amountAdd = dinamicHash.get() - prev;
+  cout << "Cantidad de acceso por Add operation: " << amountAdd << endl;
+
+  prev = dinamicHash.get();
+  const char* codigo = vu[200].getNombre();
+  dinamicHash.search(codigo);
+  int amoutSearch = dinamicHash.get() - prev;
+  cout << "Cantidad de acceso por Search operation: " << amountSearch << endl;
+  */
   deleteFilesHash();
   cout << "Finish Test "<< amount<<" - Hash" << endl;
   cout << "----------------------------------------" << endl;
@@ -58,21 +55,19 @@ void sqfTest(string amount, vector<Universitario>& data, int capacity){
   for(int i = 0; i < vu.size(); i++){
     sq.add(vu[i]);
   }
-  for(int i = 0; i < 6; i++){
-    auto start  = chrono::steady_clock::now();
-    sq.add(vu[i*100]);
-    auto end = chrono::steady_clock::now();
-    auto duration = chrono::duration_cast<chrono::microseconds>(end-start).count();
-    cout <<"Add Duration: " <<duration << endl;
-  }
-  for(int i = 0; i < 5; i++){
-    auto start  = chrono::steady_clock::now();
-    const char* codigo = vu[i*100].getNombre();
-    sq.search(codigo);
-    auto end    = chrono::steady_clock::now();
-    auto duration = chrono::duration_cast<chrono::microseconds>(end-start).count();
-    cout <<"Search duration: " <<duration << endl;
-  }
+  /*
+  int prev = sq.get();
+  sq.add(vu[100]);
+  int amountAdd = sq.get() - prev;
+  cout << "Cantidad de acceso por Add operation: " << amountAdd << endl;
+
+  prev = sq.get();
+  const char* codigo = vu[200].getNombre();
+  sq.search(codigo);
+  int amoutSearch = sq.get() - prev;
+  cout << "Cantidad de acceso por Search operation: " << amountSearch << endl;
+  */
+  
   deleteFilesSQF();
   cout << "Finish Test "<< amount<<" - SQF" << endl;
   cout << "----------------------------------------" << endl;
@@ -86,7 +81,7 @@ vector<Universitario> getUniversitarioData(string& path){
   return answer;
 }
 
-void PARSERTEST(){
+void ACCESSMEMORYTEST(){
   vector<string> files = {"../csv/Dataset/1k.csv","../csv/Dataset/5k.csv","../csv/Dataset/10k.csv",
                           "../csv/Dataset/50k.csv","../csv/Dataset/100k.csv"};
   vector<string> amount = {"1k", "5k", "10k", "50k", "100k"};
@@ -101,6 +96,6 @@ void PARSERTEST(){
 }
 
 int main(){
-  PARSERTEST();
+  ACCESSMEMORYTEST();
   return 0;
 }
